fc8485b82271203cd9981177dd7904372cdf54d9
Ihona Maria Correa de Cabo <119692820+IhonaMaria@users.noreply.github.com>
2025-05-18 16:32:35 +0200

    Delete news-data-pipeline-GCP directory

--- news-data-pipeline-GCP/weekly-fetcher/main.py
+++
@@ -1,6 +0,0 @@
-"""
-Entry point for deploying the weekly news pipeline as a Google Cloud Function.
-This file is required by GCP to locate the function handler when deploying via `gcloud functions deploy`.
-"""
-
-from adapters.gcf_function import weekly_pipeline
